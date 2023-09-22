9a4b66896d5d039ec5c895e535c86aae4eeebe27
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-09-22 20:28:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+console.log("nevil")
+const button=document.getElementById("submit");
+function callme(){
+	// event.preventDefault();
+	const name=document.getElementById("name").value;
+	const email= document.getElementById("email").value;
+	const password=document.getElementById("password").value;
+	const confirm= document.getElementById("confirm-password").value;
+	console.log(name)
+
+	if(password === confirm){
+		alert("Sign up successful!");
+		sessionstorage.setItem("name", name);
+		sessionstorage.setItem("email", email);
+		sessionstorage.setItem("password",password);
+		
+		
+	}
+	else
+	{
+		alert("Passwords do not match");
+	}
+
+
+	
+}
+
+button.addEventListener('click',callme);
+console.log("hi")
+
